ac6c43c7c94aeb5dc108ac239882dd02f5786bb0
Alexa DeWit <alexa.dewit@gmail.com>
2013-11-10 22:24:26 -0500

    Tile references completed

--- Level/Tile.cs
+++ Level/Tile.cs
@@ -19,7 +19,9 @@ namespace Level
 			TileStyle = type;
 			Contents = new List<Entity>();
 		}
-		public Tile TileToTheRight {
+		//These will return the current tile if the adjacent tile is out of the level bounds
+		//Considering changing it to a wrapping level system.
+		public Tile TileRight {
 			set;
 			get{
 				if(_tiles.GetLength(0) >= this._x){
@@ -30,5 +32,39 @@ namespace Level
 				}
 			}
 		}
+		public Tile TileLeft {
+			set;
+			get{
+				if(this._x>0){
+					return _tiles[this._x-1][this._y];
+				}
+				else{
+					return this;
+				}
+			}
+		}
+		public Tile TileAbove {
+			set;
+			get{
+				if(this._y > 0){
+					return _tiles[this._x][this._y-1];
+				}
+				else{
+					return this;
+				}
+			}
+		}
+		public Tile TileBelow {
+			set;
+			get{
+				if(_tiles.GetLength(1) >= this._y){
+					return _tiles[this._x][this._y+1];
+				}
+				else{
+					return this;
+				}
+			}
+		}
+
 	}
 }
