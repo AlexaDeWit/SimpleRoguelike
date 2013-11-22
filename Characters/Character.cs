fc07944a1078483a47bc990df5036df3b017cd63
Alexa DeWit <alexa.dewit@gmail.com>
2013-11-22 10:53:11 -0500

    Moved combat system components into the character class.
    Reteaining the CombatCapable interface to allow characters to inflict damage on things like doors(forcing it down).

--- Characters/Character.cs
+++ Characters/Character.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using BaseClasses;
+using Combat;
+using Constants;
 
 namespace Characters
 {
@@ -66,7 +68,74 @@ namespace Characters
 		//Allow a character to attack another
 		public abstract int SufferDamage(int damage);
 
+		public CombatInstance RegularAttack(ICombatCapable defender){
 
+
+			CombatInstance currentAttack = new CombatInstance();
+			int damageToInflict=0;
+
+			/*
+			 *  Bonus or pentalty in combat due to level differences
+			 *  Positive if above target, negative if below
+			 */
+			double levelDifferenceModifier = Convert.ToDouble(this.CombatLevel - defender.CombatLevel) / 100.0D;
+			double missChance = defender.EvasionChance +
+				(1.0D - this.StrikeChance) - levelDifferenceModifier;
+
+			//Damage to inflict will be progressively reduced
+			currentAttack.BaseDamage = this.CalculateDamage();
+			damageToInflict =  currentAttack.BaseDamage;
+
+			//Roll for attack
+			currentAttack.AttackRoll = this.SuccessRoll();
+			if (currentAttack.AttackRoll > missChance) {
+
+				//Determine if defender is able to block(has a shield?)
+				currentAttack.DefenderCanBlock = defender.CanBlock;
+				if(currentAttack.DefenderCanBlock){
+					//Roll to beat defender's block chance
+					currentAttack.BypassBlockRoll = this.SuccessRoll();
+					if(currentAttack.BypassBlockRoll < defender.BlockChance){
+						currentAttack.WasBlocked = true;
+						currentAttack.DamageBlocked = defender.BlockAmount;
+						damageToInflict -= currentAttack.DamageBlocked;
+					}else{
+						//Attack got through block
+						currentAttack.WasBlocked=false;
+					}
+				}else{
+				}
+				//Attack was in some way successful
+				//Do the armour damage reduction calculation
+				currentAttack.DamageReducedByArmour = ArmourDamageReduction(damageToInflict,defender.ArmourValue);
+				damageToInflict -= currentAttack.DamageReducedByArmour;
+				//Inflict the damage, and calculate how much was absorbed(determined by the defender)
+				currentAttack.DamageAbsorbed = damageToInflict - defender.SufferDamage(damageToInflict);
+				currentAttack.DamageInflicted = damageToInflict -  currentAttack.DamageAbsorbed;
+			} else {
+				currentAttack.AttackSuccess=false;
+			}
+			return currentAttack;
+		}
+		public static int ArmourDamageReduction(int baseDamage, int armour){
+			//the 1 represents the adjustment from a percentage of reduction
+			//to a ratio for multiplication
+			//IE. -30% damage taken is damage*70%
+			return (int)(baseDamage * (ArmourDamageRatio(armour)+1));
+		}
+		public static double ArmourDamageRatio (int armour)
+		{
+			double absoluteArmour =Math.Abs (armour);
+			double damageRatio = absoluteArmour / (absoluteArmour + CombatConst.ARMOUR_SCALING_RATIO);
+			//if armour mitigates 30%..
+			// -0.3 if the damage should be reduced
+			// +0.3 if the damage should be amplified.
+			if (armour >= 0) {
+				return 0.0 - damageRatio;
+			} else {
+				return damageRatio;
+			}
+		}
        	
 	}
 
