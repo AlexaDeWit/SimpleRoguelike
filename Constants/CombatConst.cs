a156b6483029794616a183f664ea2edb73497f34
Alexa DeWit <alexa.dewit@gmail.com>
2013-11-24 17:06:35 -0500

    Further XML documentation

--- Constants/CombatConst.cs
+++ Constants/CombatConst.cs
@@ -2,19 +2,41 @@ using System;
 
 namespace Constants
 {
-	/*
-	 * <summary>
-	 * Constants that are used and depended on by the combat system and related classes.
-	 * </summary>
-	 */
+	/// <summary>
+	/// Constants that are used and depended on by the combat system and related classes.
+	/// </summary>
 	public static class CombatConst
 	{
+		/// <summary>
+		/// The base hit chance of an entity agaisnt another in combat
+		/// </summary>
 		public const double BASE_HIT_CHANCE = 0.65D;
+		/// <summary>
+		/// The base chance all entities have to dodge an attack
+		/// </summary>
 		public const double BASE_EVASION_CHANCE =0.0D;
+		/// <summary>
+		/// A constant that represents a target being missed in combat
+		/// </summary>
 		public const int TARGET_MISSED=-1;
+		/// <summary>
+		/// A ratio that determines how beneficial armour is. 
+		/// The higher it is the less beneficial
+		/// </summary>
 		public const double ARMOUR_SCALING_RATIO = 150.0D;
-		public const double STRIKE_CHANCE_SCALING_RATIO = 0.35D;
+		/// <summary>
+		/// A scaling factor used for bonuses to an entity's hit chance.
+		/// </summary>
+		public const double STRIKE_CHANCE_SCALING_RATIO = 1.0 - BASE_HIT_CHANCE;
+		/// <summary>
+		/// A scaling factor to determine how beneficial agility is in increasing one's chance to hit.
+		/// The higher it is, the less beneficial
+		/// </summary>
 		public const double AGILITY_BONUS_RATIO_HIT = 10.0D;
+		/// <summary>
+		/// A scaling factor to determine how beneficial agility is in increasing one's chance to evade.
+		/// The higher it is, the less beneficial
+		/// </summary>
 		public const double AGILITY_BONUS_RATIO_EVADE = 10.0D;
 	}
 }
--- Constants/Mechanics.cs
+++ Constants/Mechanics.cs
@@ -2,21 +2,43 @@ using System;
 
 namespace Constants
 {
-	/*
-	 * <summary>
-	 * Constants to be used throughout the game that deal with game mechanics,
-	 * or simply just removing "magic numbers" from as much of the game as possible.
-	 * </summary>
-	 */
+	/// <summary>
+	/// Constants to be used throughout the game that deal with game mechanics,
+	/// or simply just removing "magic numbers" from as much of the game as possible.
+	/// </summary>
 	public static class Mechanics
 	{
+		/// <summary>
+		/// The minimum reputation one must have with a faction for them to react as friendly
+		/// </summary>
 		public const int REPUTATION_MINIMUM_FRIENDLY=100;
+		/// <summary>
+		/// The minimum reputation one must have with a faction for them to react as neutral
+		/// </summary>
 		public const int REPUTATION_MINIMUM_NEUTRAL=0;
+		/// <summary>
+		/// The maximum reputation an entity will react as hostile toward another.
+		/// </summary>
 		public const int REPUTATION_MAXIMUM_HOSTILE=-1;
+		/// <summary>
+		/// A string which represents how mass is displayed in the game
+		/// </summary>
 		public const string MASS_UNIT = "kg";
+		/// <summary>
+		/// The amount of health gained for each point of endurance
+		/// </summary>
 		public const int HEALTH_PER_ENDURANCE = 12;
+		/// <summary>
+		/// The amount of mana gained for each point of intelligence
+		/// </summary>
 		public const int MANA_PER_INTELLIGENCE =15;
+		/// <summary>
+		/// The amount of armour gained for each point of agility
+		/// </summary>
 		public const int ARMOUR_PER_AGILITY = 10;
+		/// <summary>
+		/// The point of hunger where a character will die.
+		/// </summary>
 		public const int DIE_OF_HUNGER = 100;
 	}
 }
