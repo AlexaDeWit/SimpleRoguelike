e6bc414a3fb5f738990a94f4d9c4696d3b6a1114
Alexa DeWit <alexa.dewit@gmail.com>
2013-11-27 13:23:21 -0500

    Initial planning of Enums to be used to communicate between backend and frontend.

---
+++ Enums/DIRECTION.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Enums {
+	public enum DIRECTION {
+		LEFT,
+		RIGHT,
+		UP,
+		DOWN
+	}
+}
+
---
+++ Enums/GAME_COMMAND.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Enums {
+	public enum GAME_COMMAND {
+		PLAYER_MOVE,
+		PLAYER_ATTACK,
+		PLAYER_EQUIP,
+		PLAYER_UNEQUIP
+	}
+}
+
