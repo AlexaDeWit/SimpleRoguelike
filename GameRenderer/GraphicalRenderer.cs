7afe13ac19c83a308c089a7fdb36bb3695633a52
Alexa DeWit <alexa.dewit@gmail.com>
2013-12-12 13:41:59 -0500

    * GraphicalRenderer.cs: Potential solution to the issue of data and   display abstraction
    * SimpleRoguelike.userprefs:

---
+++ GameRenderer/GraphicalRenderer.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace GameRenderer {
+	/*
+	 * Consider using a dictionary to associate tile objects with their display content,
+	 * and generally doing this for all content. it will let me abstract it from its base data
+	 * holy shit I'm a genius, no more divergent classtypes
+	 */
+	public class GraphicalRenderer : Renderer {
+		public GraphicalRenderer () {
+		}
+	}
+}
+
